8d7352922d024f78c8de820927a8ff8683b6ab2c
baogia2810 <bankaijun2880@gmail.com>
2026-02-24 13:34:44 +0700

    upload bài 4

---
+++ Bai_4.js
@@ -0,0 +1,6 @@
+let nameBook = prompt(`Moi ban nhap vao ten sach:`);
+let nameUppercase = nameBook.toLocaleUpperCase();
+let nameLowerCase = nameBook.toLowerCase();
+console.log(`Ten sach ban dau:${nameBook}`);
+console.log(`Ten sach sau khi viet thuong:${nameLowerCase}`);
+console.log(`Ten sach sau khi viet hoa:${nameUppercase}`);
