ce84062db4e80522b0737cc39f1bfdc96e757a9e
baogia2810 <bankaijun2880@gmail.com>
2026-02-24 13:33:18 +0700

    upload bài 3

---
+++ Bai_3.js
@@ -0,0 +1,4 @@
+let oldBook = +prompt(`Moi ban nhap vao so sach cu:`);
+let newBook = +prompt(`Moi ban nhap vao so sach moi:`);
+let total = oldBook + newBook;
+console.log(`Tong so sach hien tai=${total}`);
